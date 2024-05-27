eb8429f3b74f2a7d11d31d7c6e411384fc465bc2
VanessaKhourieh <vanessakhourieh@gmail.com>
2024-05-27 10:59:55 +0200

    first try using fast_api

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # demo-sso-fastapi
 FastAPI's OAuth implementation
+
+Install the requirements.txt & create venv
+
+Changes you should do in these files:
+- main : line 34 -> 41 (the urls that can access the apis)
+- json_web_token -> line 13, 14
+- config -> maybe you don't need the 'auth0_audience'
---
+++ application/main.py
@@ -0,0 +1,84 @@
+import secure
+import uvicorn
+from config import settings
+from dependencies import PermissionsValidator, validate_token
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+app = FastAPI()
+
+csp = secure.ContentSecurityPolicy().default_src("*").script_src("'unsafe-inline' https://cdn.jsdelivr.net").style_src("*").frame_ancestors("'none'")
+hsts = secure.StrictTransportSecurity().max_age(31536000).include_subdomains()
+referrer = secure.ReferrerPolicy().no_referrer()
+cache_value = secure.CacheControl().no_cache().no_store().max_age(0).must_revalidate()
+x_frame_options = secure.XFrameOptions().deny()
+
+secure_headers = secure.Secure(
+    csp=csp,
+    hsts=hsts,
+    referrer=referrer,
+    cache=cache_value,
+    xfo=x_frame_options,
+)
+
+
+@app.middleware("http")
+async def set_secure_headers(request, call_next):
+    response = await call_next(request)
+    secure_headers.framework.fastapi(response)
+    return response
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_origins=[settings.client_origin_url],
+    allow_methods=["GET"],
+    allow_headers=["Authorization", "Content-Type"],
+    max_age=86400,
+)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # Update this to restrict to specific origins if needed
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    message = str(exc.detail)
+
+    return JSONResponse({"message": message}, status_code=exc.status_code)
+
+
+@app.get("/api/messages/public", tags=["public"])
+def public():
+    return {"text": "This is a public message."}
+
+
+@app.get("/api/messages/protected", dependencies=[Depends(validate_token)])
+def protected():
+    return {"text": "This is a protected message."}
+
+
+@app.get(
+    "/api/messages/admin",
+    dependencies=[Depends(PermissionsValidator(["read:admin-messages"]))],
+)
+def admin():
+    return {"text": "This is an admin message."}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=settings.port,
+        reload=settings.reload,
+        server_header=False,
+    )
